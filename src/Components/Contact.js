f844d4f0ebc9473e387da19b14e576e9f444bcc7
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-05-08 05:30:28 +0500

    Updated contact component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import './css/Contact.css';
+import React, { useState } from "react";
+import { Form, Button, Spinner, Container, Row, Col } from "react-bootstrap";
+import './css/Contact.css'
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    // Call your API or do some other logic here...
+    setTimeout(() => {
+      setIsLoading(false);
+      setIsSubmitted(true);
+    }, 2000);
+  };
+
   return (
-    <Container className='contact'>
+    <Container className="contact">
       <Row className="justify-content-center">
         <Col lg={8}>
           <h1 className="text-center my-5">Contact Us</h1>
@@ -13,24 +37,57 @@ const Contact = () => {
       </Row>
       <Row className="justify-content-center">
         <Col lg={8}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter your name"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter your email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter your email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
             <Form.Group controlId="formBasicMessage">
               <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" rows={4} placeholder="Enter your message" />
+              <Form.Control
+                as="textarea"
+                rows={4}
+                placeholder="Enter your message"
+                name="message"
+                value={formData.message}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
-            <div className='submit'>
-              <Button variant="primary" type="submit">
-                Submit
+            <div className="submit">
+              <Button variant="primary" type="submit" disabled={isLoading}>
+                {isLoading ? (
+                  <>
+                    <Spinner animation="border" size="sm" /> Sending...
+                  </>
+                ) : (
+                  "Submit"
+                )}
               </Button>
             </div>
+            {isSubmitted && (
+              <div className="mt-3">
+                <p className="text-success">Thanks for your message!</p>
+              </div>
+            )}
           </Form>
         </Col>
       </Row>
